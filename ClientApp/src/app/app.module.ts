eed6640e224a6a4bb44689c4f5483a7a082b51c7
Tom Haumersen <tbhaum+rethink@gmail.com>
2022-09-13 19:58:30 -0400

    fix components

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -9,7 +9,7 @@ import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
 import { CounterComponent } from './counter/counter.component';
 import { FetchDataComponent } from './fetch-data/fetch-data.component';
-import { UploadCsvComponent } from './fetch-data/fetch-data.component';
+import { UploadCsvComponent } from './upload-csv/upload-csv.component';
 
 @NgModule({
   declarations: [
