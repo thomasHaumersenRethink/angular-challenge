27b57b9798826f4b3cfd7abe453dcbb9fac0f99e
Tom Haumersen <tbhaum+rethink@gmail.com>
2022-09-13 19:58:40 -0400

    finsh up upload

--- ClientApp/src/app/upload-csv/upload-csv.component.ts
+++ ClientApp/src/app/upload-csv/upload-csv.component.ts
@@ -2,31 +2,44 @@ import { Component, Inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 @Component({
-  selector: 'app-upload-csv',
-  templateUrl: './upload-csv.component.html'
+    selector: 'app-upload-csv',
+    templateUrl: './upload-csv.component.html'
 })
-  const [fileSelected, setFileSelected] = UseState<File>() 
-  export class UploadCsvComponent {
-  constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
-    
-  }
+export class UploadCsvComponent {
+    public successFileName?: string;
+    public error: boolean;
+    private selectedFile?: File;
+    private http: HttpClient;
+    private baseUrl: string;
 
+    constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
+        this.http = http;
+        this.baseUrl = baseUrl;
+        this.error = false;
+    }
 
-  const upload(){
-    const formData = new FormData();
+    public setSelectedFile(event: any) {
+        const file: File = event.target.files[0];
+        this.selectedFile = file;
+    }
 
-    formData.append('File', selectedFile);
-
-    fetch(
-        'https://freeimage.host/api/1/upload?key=<YOUR_API_KEY>',
-        {
-            method: 'POST',
-            body: formData,
+    public upload() {
+        console.log("in function 2")
+        if (!this.selectedFile) {
+            console.log("file not set")
+            return;
         }
-    )
-  }
-}
+        const formData = new FormData();
 
-interface UploadCsvState {
-  selectFile: File;
+        formData.append('File', this.selectedFile);
+
+        this.http.post(this.baseUrl + "patients", formData).subscribe(result => {
+            this.successFileName = this.selectedFile?.name;
+            this.error = false;
+        }, error => {
+            this.error = true; console.error(error)
+            this.successFileName = undefined;
+        });
+
+    }
 }
